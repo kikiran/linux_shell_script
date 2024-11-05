dbf35a2a22edf7adae4f6634d005b54ff7278240
kikiran <kiran.m79955@gmail.com>
2024-11-05 12:46:42 +0530

    added if condition in the app

--- deploy_mernapp.sh
+++ deploy_mernapp.sh
@@ -2,19 +2,28 @@
 ######################################
 #Author: KIRANKUMAR
 #Date: 05-Nov-2024
-#version: V1
+#version: V2
 ## This script deploy MERN Stack App
 ######################################
-set -X
+set -ex
 
 #Installation of git
 
-sudo apt-get install git
+if command -v git >/dev/null 2>&1; then
+echo "Git is already installed"
+else 
+
+sudo apt-get install -y git
+fi
 
 # Installation of NodeJS
+if command -v nvm >/ev/null 2>&1; then
+echo "nvm already Installed" 
+else
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 source ~/.bashrc
 nvm install --lts
+fi
 
 #Clone Project in server
 git clone https://github.com/kikiran/mern_crud_app.git
