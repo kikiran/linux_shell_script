28f27b568dbc1e18a6ff0b03747a67b67c08519f
kikiran <kiran.m79955@gmail.com>
2025-08-18 19:10:14 +0530

    added version 2

--- day-03/nodehealth_script.sh
+++ day-03/nodehealth_script.sh
@@ -4,10 +4,12 @@
 # Author: KIRAN
 # Date: 18-08-2025
 # This script will display the system health
-# Version-1
+# Version-2
 ################################
 
 set -x #debug mode
+set -e #any error will reflect
+set -o pipefail
 
 df -h
 
