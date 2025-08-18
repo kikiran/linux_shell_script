c07420c45abbcee8f9b5e2c08ddec96d6567c2f0
kikiran <kiran.m79955@gmail.com>
2025-08-18 19:42:44 +0530

    updated awk and grep commands

--- day-04/error_log_script.sh
+++ day-04/error_log_script.sh
@@ -3,7 +3,7 @@
 ##################################
 # Author: KIRAN
 # Date: 18-08-2025
-# version: 1
+# version: 2
 # This script was used to find the error logs in the file
 ###################################
 
@@ -12,3 +12,5 @@ set -e
 set -o pipefail
 
 cat error.log | grep ERROR
+
+curl https://raw.githubusercontent.com/kikiran/linux_shell_script/refs/heads/main/day-04/error.log | grep INFO | awk -F',' '{print $5}'
