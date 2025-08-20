b57d17018f86e6cd5d73393fdf13bb30629b1412
kikiran <kiran.m79955@gmail.com>
2025-08-20 14:23:16 +0530

    added if else condition

---
+++ day-05/ifelse_script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#############################
+# Author: KIRAN
+# Date: 19-08-2025
+# Version: 1
+# This script is for if and ifelse using shell script
+#############################
+
+a=100
+b=20
+
+if [ $a -gt $b ]
+then
+    echo 'A is greater than B'
+else
+    echo 'B is greater than A'
+fi
