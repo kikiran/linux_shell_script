3fc216f3e18000f552848c4dab5d27038427bc5e
kikiran <kiran.m79955@gmail.com>
2025-08-20 14:38:30 +0530

    for loop added

---
+++ day-06/forloop_script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#############################
+# Author: KIRAN
+# Date: 20-08-2025
+# Version: 1
+# This script is for loop using shell script
+#############################
+
+for i in {1..100}
+do 
+    echo $i
+done
